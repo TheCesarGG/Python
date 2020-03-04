fbcd266ac4681203d1257e8b7b549087c02ddea0
TheCesarGG <61486007+TheCesarGG@users.noreply.github.com>
2020-03-03 22:25:15 -0600

    Activity 01 – Python introduction
    Create a simple program that prints a profile by using variables and formatting strings.

---
+++ 1.py
@@ -0,0 +1,17 @@
+First_Name= "Cesar"
+Second_Name= 'Obed'
+Last_Name= "Garcia Gomez"
+Age= "19 years"
+Height= "1.80m"
+Id= "193111820"
+
+print('Name: {} , {} , {} , {} , {} , {}  '.format(Last_Name,First_Name,Second_Name,Age,Height,Id))
+
+print('Name:        {}' .format(First_Name))
+print('Second Name: {}' .format(Second_Name))
+print('Last Name:   {}' .format(Last_Name))
+print('Age:         {}' .format(Age))
+print('Height:      {}' .format(Height))
+print('Id:          {}' .format(Id))
+
+print('{:13}{}' .format('Id','193111820'))
