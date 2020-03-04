663cfa85135259265cb37021f5dfb3b21ab16813
TheCesarGG <61486007+TheCesarGG@users.noreply.github.com>
2020-03-03 22:27:45 -0600

    Activity 02 – Simple AI
    Create a simple program that asks for user data and changes the output based on that data.

---
+++ 2.py
@@ -0,0 +1,30 @@
+
+name  = input ("What is your name? ")
+print("Hi!!", name)
+print("What is your age? ")
+age =float (input() )
+print("What is your height? ")
+height =float (input() )
+if   age >= 18:
+ print("You are  :",age,"years")
+ print("You can now vote" )
+else:
+   print("You are  :",age,"years")
+   print("You can't now vote" )
+if height >=170:
+   print("You are tall")
+elif height >=160:
+   print("You are medium")
+else:
+   print ("You are short")
+   
+  
+
+
+
+
+
+
+
+
+
